084d242c962afebd4c67ffe313cb82e8e18a93e5
ferryrules <ferris@ferryrules.com>
2019-05-13 11:36:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(drivers, str) {
+  return drivers.filter(function(driver) {
+   return driver.toLowerCase() === str.toLowerCase()
+  });
+}
+
+function fuzzyMatch(drivers, str) {
+  let strLen = str.length;
+  return drivers.filter(function(driver) {
+    return driver.substring(0,strLen) === str
+  });
+}
+
+
+function matchName(drivers, str) {
+  return drivers.filter(function(driver){return driver.name.toLowerCase() === str.toLowerCase()})
+}
